bd15f8e825fc4a20a11091da4164473eadb64743
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-09-20 17:44:38 +0900

    3일차 과제 1 완성

--- day3/assignment1/lib/main.dart
+++ day3/assignment1/lib/main.dart
@@ -1,125 +1,147 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, must_be_immutable
+// ignore: prefer_const_literals_to_create_immutables
+import 'package:assignment1/ItemTile.dart';
+import 'package:assignment1/models/tea.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List<Tea> teas = [
+    Tea("골든 미모사 그린 티", "Golden Mimosa Green Tea", "6100원",
+        'assets/imgs/item_drink1.jpeg'),
+    Tea("블랙 햅쌀 고봉 라떼", "Black Rice Latte", "6300원",
+        'assets/imgs/item_drink2.jpeg'),
+    Tea("아이스 블랙 햅쌀 고봉 라떼", "Iced Black Rice Latte", "6100원",
+        'assets/imgs/item_drink3.jpeg'),
+    Tea("스타벅스 튜메릭 라떼", "Starbucks Turmeric Latte", "6100원",
+        'assets/imgs/item_drink4.jpeg'),
+    Tea("아이스 스타벅스 튜메릭 라떼", "Iced Starbucks Turmeric Latte", "6200원",
+        'assets/imgs/item_drink5.jpeg'),
+  ];
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: SafeArea(
+        child: Scaffold(
+          // resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Icon(Icons.arrow_back_ios),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(Icons.search),
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, bottom: 16),
+                  child: Text(
+                    'NEW',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: teas.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 16.0),
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage(teas[index].imgUrl),
+                                radius: 48,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(teas[index].name,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600)),
+                                Text(
+                                  teas[index].EngName,
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w200),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Text(teas[index].price,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w600)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.card_giftcard), label: 'Pay'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.local_cafe), label: 'Order'),
+              BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.other_houses), label: 'other'),
+            ],
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Colors.green,
+            currentIndex: 2,
+            // unselectedItemColor: Colors.grey,
+            // selectedItemColor: Colors.green,
+          ),
+          bottomSheet: Container(
+            height: 60,
+            color: Colors.black87,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Text(
+                    "주문할 매장을 선택해 주세요.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.expand_more,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ day3/assignment1/lib/models/tea.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Tea {
+  final String name;
+  final String EngName;
+  final String price;
+  final String imgUrl;
+
+  Tea(this.name, this.EngName, this.price, this.imgUrl);
+}
