1393f354e132a73513bc6e2c1c8c80044317945f
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-09-30 10:05:54 +0900

    8일차 과제 1 완성

--- day8/assignment1/lib/main.dart
+++ day8/assignment1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +10,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('1번과제 ')),
+        body: Center(
+          child: TextButton(
+            onPressed: sendPostRequest,
+            child: Text('POST 통신'),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+// 정답 5292304 - result = 119
+Future<void> sendPostRequest() async {
+  final dio = Dio();
+  int result = 101;
+  for (result; result < 151; result++) {
+    String url = "https://sniperfactory.com/sfac/http_assignment_$result";
+    try {
+      var response = await dio.post(
+        url,
+        options: Options(headers: {
+          'User-Agent': 'SniperFactoryBrowser',
+          'authorization': 'bearer ez-1234'
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('성공! ${result}');
+        print('Response body: ${response.data}');
+      } else {
+        print('실패');
+        print(result);
+      }
+    } catch (e) {
+      print('${result}');
+      print(e.toString());
+    }
   }
 }
