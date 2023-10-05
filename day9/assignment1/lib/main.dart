5e84059c11d203374e7246834d01fa3c9015e6a5
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-10-05 15:15:58 +0900

    10일차 과제 시작

---
+++ day9/assignment1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:assignment2/Pages/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "neo"),
+      home: HomePage(),
+    );
+  }
+}
